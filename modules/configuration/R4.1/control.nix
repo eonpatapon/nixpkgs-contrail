a7794192bcbd60165bf48dab83549020583c417f
Jean-Philippe Braun <eon@patapon.info>
2019-01-02 11:05:48 +0100

    Fix config

--- modules/configuration/R4.1/control.nix
+++ modules/configuration/R4.1/control.nix
@@ -10,6 +10,7 @@ pkgs.writeTextFile {
     use_syslog = 1
 
     hostip = 0.0.0.0
-    collectors = 127.0.0.1:8086
+    # FIXME: when 127.0.0.1:8086 it will not connect to collector :s
+    collectors = 192.168.1.1:8086
   '';
 }
